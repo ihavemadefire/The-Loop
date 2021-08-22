dba6a626234cd865cb48b5223dc560df769c67a0
Jacob <jacobmeide@gmail.com>
2021-08-21 20:25:35 -0500

    Create Tables Fixes #7

--- backend/the_loop_backend/api/models.py
+++ backend/the_loop_backend/api/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+from django.db.models.fields.related import ManyToManyField
 
 # Create your models here.
+
+
+class District(models.Model):
+    district = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.district + " District"
+
+
+class Amenities(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Type(models.Model):
+    type = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.type
+
+class SubType(models.Model):
+    SubType = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.SubType
+
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    description = models.TextField()
+    phone_number = models.CharField(max_length=14)
+    price_choices = [
+        ('$', '$'),
+        ('$$', '$$'),
+        ('$$$', '$$$'),
+        ('$$$$', '$$$$')
+    ]
+    price = models.CharField(max_length=5, default=1, choices=price_choices)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, null=True)
+    amenities = ManyToManyField(Amenities)
+    type = models.ForeignKey(Type, on_delete=CASCADE)
+    subtype = ManyToManyField(SubType)
+    
+    def __str__(self):
+        return self.name
