55dd02755b87914012878a62f08baaaac9e5239b
Jacob <jacobmeide@gmail.com>
2021-08-29 06:25:19 -0500

    update settings

--- backend/the_loop_backend/the_loop_backend/settings.py
+++ backend/the_loop_backend/the_loop_backend/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.88.191.13']
 
 
 # Application definition
