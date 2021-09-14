14f95dcec08fb2f5ba70c5b3d4ba54b92c7af6b3
Jacob <jacobmeide@gmail.com>
2021-09-14 16:33:40 -0500

    add when field to sceme

--- backend/the_loop_backend/api/schema.py
+++ backend/the_loop_backend/api/schema.py
@@ -13,6 +13,7 @@ class DistrictType(DjangoObjectType):
 class PlaceType(DjangoObjectType):
     class Meta:
         model = Place
+        interfaces = (relay.Node,)
         fields = ('id', 'name', 'attribution', 'short_description', 'address', 'description', 'phone_number', 'price',
                   'district', 'amenities', 'open_now', 'type', 'subtype', 'lat', 'long', 'image')
 
@@ -38,13 +39,14 @@ class SubTypeType(DjangoObjectType):
 class TimeType(DjangoObjectType):
     class Meta:
         model = Time
+        interfaces = (relay.Node,)
         fields = ('id', 'day', 'time', 'open_close', 'place')
 
 
 class EventType(DjangoObjectType):
     class Meta:
         model = Event
-        fields = ('id' ,'name', 'attribution', 'short_description', 'type', 'venue', 'recurring', 'active', 'description', 'tix_required', 'tix_link', 'image')
+        fields = ('id' ,'name', 'attribution', 'short_description', 'when','type', 'venue', 'recurring', 'active', 'description', 'tix_required', 'tix_link', 'image')
 
 
 class TypeEventType(DjangoObjectType):
@@ -59,6 +61,15 @@ class EventDateType(DjangoObjectType):
         fields = ('id', 'date', 'time', 'where')
 
 
+class TimePlaceUnion(graphene.Union):
+    class Meta:
+        types = (PlaceType, TimeType)
+
+class PlacesType(graphene.ObjectType):
+    class Meta:
+        fields = ('id', 'name', 'attribution', 'short_description', 'address', 'description', 'phone_number', 'price',
+                  'district', 'amenities', 'open_now', 'type', 'subtype', 'lat', 'long', 'image', 'times')
+
 class Query(graphene.ObjectType):
     all_places = graphene.List(PlaceType)
     all_events = graphene.List(EventType)
@@ -66,6 +77,9 @@ class Query(graphene.ObjectType):
     one_event = graphene.Field(EventType, id=graphene.Int())
     all_times = graphene.List(TimeType)
     all_times_place = graphene.List(TimeType, id=graphene.ID())
+    all_dates_event = graphene.List(EventDateType, id=graphene.ID())
+    all_places_and_times = graphene.List(TimePlaceUnion)
+    PlacesType = graphene.List(PlaceType)
 
     def resolve_all_places(self, info):
         return Place.objects.order_by('id')
@@ -85,5 +99,21 @@ class Query(graphene.ObjectType):
     def resolve_all_times_place(self, info, id):
         return Time.objects.filter(place=id)
 
+    def resolve_all_dates_event(self, info, id):
+        return EventDate.objects.filter(id=id)
+
+    def resolve_all_places_and_times(self, info):
+        queryset1 = Place.objects.all()
+        queryset2 = Time.objects.all()
+        places = queryset1.order_by('id').values()
+        times = queryset2.values()
+        for i in places:
+            place_id = (i['id'])
+            ind_of_times = next((i for (i, d) in enumerate(times) if d["place_id"] == place_id), 0)
+            i['times']=times[ind_of_times]
+        print(places)
+        print(type(queryset1))
+        print(type(places))
+        return [*queryset2, *queryset1 ]
 
 schema = graphene.Schema(query=Query)
