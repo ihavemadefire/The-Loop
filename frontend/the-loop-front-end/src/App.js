33fa1256398d956df9cd1ff606f077cb86ee2acf
Beardocracy <travisjbearden@gmail.com>
2021-09-16 10:45:59 -0500

    Reverted to http

--- frontend/the-loop-front-end/src/App.js
+++ frontend/the-loop-front-end/src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer.js';
 
 
 const client = new ApolloClient({
-	uri: "https://3.88.191.13:8000/___graphql"
+	uri: "http://3.88.191.13:8000/___graphql"
 });
 
 function App() {
