e63ea9a9254c2d278603324ab3b6ea3c1a332385
OctopusHugz <colsonscott53@gmail.com>
2021-09-20 17:03:04 +0000

    Removed all mobile files from top level

--- client.jsx
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://44.198.144.202:8000/___graphql",
-  cache: new InMemoryCache(),
-});
-
-export default client;
